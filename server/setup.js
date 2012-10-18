d62584775976c93b37dfff5696033110b3bd3cc2
Daniel Graziotin <dgraziotin@task3.cc>
2012-10-18 10:59:53 +0200

    Initial Meteor v0.5.0 support

--- server/setup.js
+++ server/setup.js
@@ -3,6 +3,46 @@ var Concerns = new Meteor.Collection("Concerns");
 var Alphas = new Meteor.Collection("Alphas");
 var States = new Meteor.Collection("States");
 
+// Create a collection where users can only modify documents that
+// they own. Ownership is tracked by an 'owner' field on each
+// document. All documents must be owned by the user that created
+// them and ownership can't be changed. Only a document's owner
+// is allowed to delete it, and the 'locked' attribute can be
+// set on a document to prevent its accidental deletion.
+
+States.allow({
+  update: function (userId, docs, fields, modifier) {
+    // can only change your own documents
+    return _.all(docs, function(doc) {
+      return true;//doc.owner === userId;
+    });
+  },
+  //fetch: ['owner']
+});
+
+Alphas.allow({
+  update: function (userId, docs, fields, modifier) {
+    // can only change your own documents
+    return _.all(docs, function(doc) {
+      return true;//doc.owner === userId;
+    });
+  },
+  //fetch: ['owner']
+});
+
+Concerns.allow({
+  update: function (userId, docs, fields, modifier) {
+    // can only change your own documents
+    return _.all(docs, function(doc) {
+      return true;//doc.owner === userId;
+    });
+  },
+  //fetch: ['owner']
+});
+
+
+
+
 Meteor.publish("Kernel", function() {
     return Kernel.find({},{});
   });
