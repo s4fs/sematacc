7438835b6ea50a20bde1f483d1ed0b7f7620185c
Daniel Graziotin <daniel@ineed.coffee>
2016-05-17 10:31:47 +0200

    Added new app routes

---
+++ client/lib/routes.js
@@ -0,0 +1,61 @@
+/**
+ * Semat Essence Accelerator
+ * Copyright (C) 2013 Daniel Graziotin. All Rights Reserved.
+ * Licensed under the BSD 3-Clause. See the LICENSE File for details.
+ */
+
+/***
+ * Routing
+ */
+
+/*
+Meteor.Router.beforeRouting = function() {
+    Session.set('selectedProjectId', null);
+    Session.set('selectedProjectName', null);
+    Session.set('message', null);
+    Session.set('demoMode', null);
+}
+*/
+
+Router.configure({
+  layoutTemplate: 'ApplicationLayout'
+});
+
+Router.route('/', function () {
+  this.layout('ApplicationLayout');
+  this.render('home');
+});
+
+Router.route('/demo', function () {
+    var id = this.params._id;
+    if (Meteor.loggingIn() || Meteor.user()) {
+        Session.set('demoMode', null);
+        this.render('home');
+    }
+    this.wait(Meteor.subscribe('Projects', id));
+    if (this.ready()) {
+        Session.set('demoMode', true);
+        this.render('kernel');
+    } else {
+        this.render('loading');
+    }
+});
+
+
+Router.route('/p', function () {
+  this.render('projects');
+});
+
+Router.route('/p/:_id', function () {
+    var id = this.params._id;
+    this.wait(Meteor.subscribe('Projects', id));
+    if (this.ready()) {
+        Session.set('selectedProjectId', id);
+        this.render('kernel');
+    } else {
+        this.render('loading');
+    }
+});
+
+
+
